e4a5b65e810b332e9884ea6f53c37eb4e176e23d
pauljoo <huiyaojiang@gmail.com>
2018-01-27 17:05:11 +0800

    Initial commit

---
+++ labs-spring-boot/src/main/java/info/xpanda/labs/controller/SampleController.java
@@ -0,0 +1,14 @@
+package info.xpanda.labs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+}

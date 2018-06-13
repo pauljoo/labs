aa88ff212dfef6c1596e1de5dede5347418d5bf7
Paul Joo <jianghuiyao@outlook.com>
2018-06-13 19:47:07 +0800

    修改gateway目录结构

--- labs-spring-cloud/labs-spring-cloud-gateway/src/main/java/info/xpanda/labs/spring/cloud/gateway/GatewayApplication.java
+++ labs-spring-cloud/labs-spring-cloud-gateway/src/main/java/info/xpanda/labs/spring/cloud/gateway/GatewayApplication.java
@@ -5,8 +5,10 @@ import info.xpanda.cbb.spring.boot.zuul2.Zuul2StartEvent;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApplication implements CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
@@ -18,6 +20,7 @@ public class GatewayApplication implements CommandLineRunner {
     //access command line arguments
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("");
         //do something
     }
 }
